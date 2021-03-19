2e941bbdd985200857ec30140d7f58297a16bbf6
Zelyder < . zelyder0@gmail.com>
2021-03-20 00:41:54 +0300

    Added search states

--- app/src/main/java/com/zelyder/stocksapp/presentation/searchscreen/SearchStocksListFragment.kt
+++ app/src/main/java/com/zelyder/stocksapp/presentation/searchscreen/SearchStocksListFragment.kt
@@ -1,11 +1,15 @@
 package com.zelyder.stocksapp.presentation.searchscreen
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,9 +18,11 @@ import com.zelyder.stocksapp.R
 import com.zelyder.stocksapp.domain.models.Stock
 import com.zelyder.stocksapp.presentation.stockslist.StockListItemClickListener
 import com.zelyder.stocksapp.presentation.stockslist.StocksListAdapter
-import com.zelyder.stocksapp.presentation.stockslist.StocksListViewModel
 import com.zelyder.stocksapp.viewModelFactoryProvider
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 
+@ExperimentalCoroutinesApi
 class SearchStocksListFragment : Fragment(), StockListItemClickListener {
 
     private val viewModel: SearchViewModel by viewModels {
@@ -26,6 +32,9 @@ class SearchStocksListFragment : Fragment(), StockListItemClickListener {
 
     private var recyclerView: RecyclerView? = null
     private var pbSearch: ProgressBar? = null
+    private var tvErrorText: TextView? = null
+    private var ivNoConnection: ImageView? = null
+    private  lateinit var stocksAdapter: StocksListAdapter
 
 
     override fun onCreateView(
@@ -35,25 +44,25 @@ class SearchStocksListFragment : Fragment(), StockListItemClickListener {
         return inflater.inflate(R.layout.fragment_search_stocks_list, container, false)
     }
 
+    @FlowPreview
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         recyclerView = view.findViewById(R.id.rvStocksSearch)
         pbSearch = view.findViewById(R.id.pbSearch)
+        tvErrorText = view.findViewById(R.id.tvErrorTextSearch)
+        ivNoConnection = view.findViewById(R.id.ivNoConnection)
 
         recyclerView?.layoutManager = LinearLayoutManager(
             view.context,
             LinearLayoutManager.VERTICAL,
             false
         )
-        recyclerView?.adapter = StocksListAdapter(this)
+        stocksAdapter = StocksListAdapter(this)
+        recyclerView?.adapter = stocksAdapter
 
-        viewModel.stocksList.observe(this.viewLifecycleOwner) {
-            (recyclerView?.adapter as? StocksListAdapter)?.apply {
-                bindStocks(it)
-                pbSearch?.isVisible = false
-            }
-        }
+        viewModel.stocksList.observe(this.viewLifecycleOwner,  ::handleSearchListResult)
+        viewModel.searchState.observe(viewLifecycleOwner, ::handleLoadingState)
         arguments?.getString(KEY_QUERY)?.let { viewModel.searchStock(it) }
     }
 
@@ -62,6 +71,54 @@ class SearchStocksListFragment : Fragment(), StockListItemClickListener {
         recyclerView = null
         pbSearch = null
 
+    }
+
+    private fun handleLoadingState(state: SearchState) {
+        when (state) {
+            Loading -> {
+                pbSearch?.visibility = View.VISIBLE
+            }
+            Ready -> {
+                pbSearch?.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun handleSearchListResult(result: SearchResult) {
+        when (result) {
+            is ValidResult -> {
+                    stocksAdapter.bindStocks(result.result)
+            }
+            is ErrorResult -> {
+                stocksAdapter.bindStocks(emptyList())
+                tvErrorText?.visibility = View.VISIBLE
+                recyclerView?.visibility = View.GONE
+                ivNoConnection?.visibility = View.VISIBLE
+                tvErrorText?.setText(R.string.tv_searched_error_text)
+                Log.e(this::class.java.name, "Something went wrong.", result.e)
+            }
+            is EmptyResult -> {
+                stocksAdapter.bindStocks(emptyList())
+                tvErrorText?.visibility = View.VISIBLE
+                recyclerView?.visibility = View.GONE
+                ivNoConnection?.visibility = View.GONE
+                tvErrorText?.setText(R.string.tv_searched_empty_result_text)
+            }
+            is EmptyQuery -> {
+                stocksAdapter.bindStocks(emptyList())
+                tvErrorText?.visibility = View.VISIBLE
+                recyclerView?.visibility = View.GONE
+                ivNoConnection?.visibility = View.GONE
+                tvErrorText?.setText(R.string.tv_searched_placeholder_text)
+            }
+            is TerminalError -> {
+                Toast.makeText(
+                    activity,
+                    getString(R.string.tv_searched_error_unknown_text),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/com/zelyder/stocksapp/presentation/searchscreen/SearchViewModel.kt
+++ app/src/main/java/com/zelyder/stocksapp/presentation/searchscreen/SearchViewModel.kt
@@ -1,29 +1,67 @@
 package com.zelyder.stocksapp.presentation.searchscreen
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.zelyder.stocksapp.domain.models.Stock
 import com.zelyder.stocksapp.domain.repositories.SearchRepository
 import com.zelyder.stocksapp.domain.repositories.StocksListRepository
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import java.util.*
+import java.util.concurrent.CancellationException
 
+@ExperimentalCoroutinesApi
 class SearchViewModel(private val searchRepository: SearchRepository): ViewModel() {
     private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
         Log.e(this::class.java.simpleName, "CoroutineExceptionHandler:$throwable")
     }
 
-    private val _stocksList = MutableLiveData<List<Stock>>()
+    private val _searchState = MutableLiveData<SearchState>()
+    val searchState: LiveData<SearchState> get() = _searchState
 
-    val stocksList: LiveData<List<Stock>> get() = _stocksList
+
+    private val queryChannel = BroadcastChannel<String>(Channel.CONFLATED)
+
+    @FlowPreview
+    val stocksList: LiveData<SearchResult> get() = queryChannel
+        .asFlow()
+        .onEach { _searchState.value = Loading }
+        .mapLatest {
+            if (it.isEmpty()) {
+                EmptyQuery
+            } else {
+                try {
+                    val result = searchRepository.searchStock(it.toUpperCase(Locale.ROOT))
+                    if( result.isEmpty()) {
+                        EmptyResult
+                    } else {
+                        ValidResult(result)
+                    }
+                } catch (e: Throwable) {
+                    if (e is CancellationException) {
+                        throw e
+                    } else {
+                        Log.w(this::class.java.name, e)
+                        ErrorResult(e)
+                    }
+                }
+            }
+        }
+        .onEach { _searchState.value = Ready }
+        .catch { emit(TerminalError) }
+        .asLiveData(viewModelScope.coroutineContext)
+
+
 
     fun searchStock(query: String) {
+
         viewModelScope.launch(coroutineExceptionHandler) {
-            _stocksList.value = searchRepository.searchStock(query.toUpperCase(Locale.ROOT))
+            queryChannel.send(query)
         }
     }
 
