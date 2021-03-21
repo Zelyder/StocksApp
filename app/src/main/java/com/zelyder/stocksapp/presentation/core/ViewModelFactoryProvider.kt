3c0d3548cf6d69f755b31dfa80854cd5a81f05b3
Zelyder <31486756+Zelyder@users.noreply.github.com>
2021-03-21 18:18:25 +0300

    Revert "Feature/search"

--- app/src/main/java/com/zelyder/stocksapp/presentation/core/ViewModelFactoryProvider.kt
+++
@@ -1,5 +0,0 @@
-package com.zelyder.stocksapp.presentation.core
-
-interface ViewModelFactoryProvider {
-    fun viewModelFactory(): ViewModelFactory
-}
--- app/src/main/java/com/zelyder/stocksapp/presentation/stockslist/StockListItemClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.zelyder.stocksapp.presentation.stockslist
-
-import com.zelyder.stocksapp.domain.models.Stock
-
-interface StockListItemClickListener {
-    fun onClickFavourite(stock: Stock)
-}
