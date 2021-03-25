4dc2e6553ac1bee82e370335a255ab09803717ad
Zelyder < . zelyder0@gmail.com>
2021-03-25 19:50:08 +0300

    Added Web socket

--- app/src/main/java/com/zelyder/stocksapp/data/network/StocksNetworkModule.kt
+++ app/src/main/java/com/zelyder/stocksapp/data/network/StocksNetworkModule.kt
@@ -1,6 +1,7 @@
 package com.zelyder.stocksapp.data.network
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.zelyder.stocksapp.data.FINNHUB_API_KEY
 import com.zelyder.stocksapp.data.FINNHUB_BASE_URL
 import com.zelyder.stocksapp.data.MBOUM_BASE_URL
 import com.zelyder.stocksapp.data.network.apis.FinnhubApi
@@ -8,6 +9,9 @@ import com.zelyder.stocksapp.data.network.apis.MboumApi
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.WebSocket
 import retrofit2.Retrofit
 import retrofit2.create
 
@@ -28,7 +32,19 @@ class StocksNetworkModule: StocksNetworkClient {
         .addConverterFactory(jsonFormat.asConverterFactory("application/json".toMediaType()))
         .build()
 
+    private val client = OkHttpClient()
+    private val request = Request.Builder().url("wss://ws.finnhub.io?token=$FINNHUB_API_KEY").build()
+    private val listener = PriceWebSocketListener()
+
+
+
     override fun finnhubApi(): FinnhubApi = finnhubRetrofit.create()
 
     override fun mboumApi(): MboumApi = mboumRetrofit.create()
+
+    override fun openWebSocket(): WebSocket = client.newWebSocket(request, listener)
+
+    override fun closeWebSocket() {
+        client.dispatcher.executorService.shutdown()
+    }
 }
