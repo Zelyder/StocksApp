ba3cd0fc50a9241e6de44b595908112d8d296b1b
Zelyder < . zelyder0@gmail.com>
2021-04-03 20:57:05 +0300

    Added tab layout

--- app/src/main/java/com/zelyder/stocksapp/presentation/details/DetailsFragment.kt
+++ app/src/main/java/com/zelyder/stocksapp/presentation/details/DetailsFragment.kt
@@ -1,49 +1,33 @@
 package com.zelyder.stocksapp.presentation.details
 
 import android.os.Bundle
-import android.util.TypedValue
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.Description
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.chip.ChipGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.zelyder.stocksapp.R
-import com.zelyder.stocksapp.domain.enums.SelectedItem
-import com.zelyder.stocksapp.presentation.core.toDeltaString
-import com.zelyder.stocksapp.presentation.core.toPriceString
-import com.zelyder.stocksapp.viewModelFactoryProvider
-import kotlinx.android.synthetic.main.fragment_details.*
+import java.lang.IllegalArgumentException
 
 
 class DetailsFragment : Fragment() {
+
     private val args: DetailsFragmentArgs by navArgs()
 
-    private val viewModel: DetailsViewModel by viewModels {
-        viewModelFactoryProvider()
-            .viewModelFactory()
-    }
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+    private lateinit var ivBack: ImageView
 
     private lateinit var tvTicker: TextView
     private lateinit var tvCompanyName: TextView
-    private lateinit var tvCurrentPrice: TextView
-    private lateinit var tvDayDelta: TextView
-    private lateinit var ivBack: ImageView
     private lateinit var ivFav: ImageView
-    private lateinit var btnBuy: MaterialButton
-    private lateinit var chart: LineChart
-    private lateinit var cgScaleChart: ChipGroup
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -56,128 +40,40 @@ class DetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initViews(view)
-
-        val typedValue = TypedValue()
-        requireContext().theme.resolveAttribute(R.attr.colorOnSecondary, typedValue, true)
-
-        viewModel.stockCandle.observe(this.viewLifecycleOwner) {
-            val entries: MutableList<Entry> = mutableListOf()
-            for (i in it.closePrices.indices) {
-                entries.add(Entry(it.timeStamps[i].toFloat(), it.closePrices[i]))
-            }
-            val dataSet = LineDataSet(entries, "Label")
-            dataSet.apply {
-                color = typedValue.data
-                valueTextColor = typedValue.data
-                setDrawCircles(false)
-                setDrawValues(false)
-                setDrawFilled(true)
-                fillDrawable =
-                    ContextCompat.getDrawable(requireContext(), R.drawable.chart_fill_gradient)
-                setDrawHorizontalHighlightIndicator(false)
-                highLightColor = typedValue.data
-                highlightLineWidth = 1.0f
-            }
-
-
-            val lineData = LineData(dataSet)
-            chart.data = lineData
-            chart.invalidate()
-
-        }
-        viewModel.uploadChart(args.stock.ticker, SelectedItem.MONTH)
-        cMonth.isChecked = true
-        // There should have been a web socket subscription, but something went wrong :(
-//        viewModel.subscribeToSocketEvents(args.stock.ticker)
     }
 
 
     private fun initViews(view: View) {
+        viewPager = view.findViewById(R.id.vpDetails)
+        tabLayout = view.findViewById(R.id.tabLayoutDetails)
+
         tvTicker = view.findViewById(R.id.tvTickerDetails)
         tvCompanyName = view.findViewById(R.id.tvCompanyNameDetails)
-        tvCurrentPrice = view.findViewById(R.id.tvCurrentPriceDetails)
-        tvDayDelta = view.findViewById(R.id.tvDayDeltaDetails)
-        ivBack = view.findViewById(R.id.ivBackDetails)
         ivFav = view.findViewById(R.id.ivFavDetails)
-        btnBuy = view.findViewById(R.id.btnBuy)
-        chart = view.findViewById(R.id.chart)
-        cgScaleChart = view.findViewById(R.id.cgScaleChart)
-
-        var selectedItem: SelectedItem? = null
-        var showTime = true
-        cgScaleChart.setOnCheckedChangeListener { _, checkedId ->
-            when (checkedId) {
-                R.id.cDay -> {
-                    selectedItem = SelectedItem.DAY
-                    showTime = true
-                }
-                R.id.cWeek -> {
-                    selectedItem = SelectedItem.WEEK
-                    showTime = true
-                }
-                R.id.cMonth -> {
-                    selectedItem = SelectedItem.MONTH
-                    showTime = false
-                }
-                R.id.cSixMonths -> {
-                    selectedItem = SelectedItem.SIX_MONTHS
-                    showTime = false
-                }
-                R.id.cYear -> {
-                    selectedItem = SelectedItem.YEAR
-                    showTime = false
-                }
-            }
-            selectedItem?.let { viewModel.uploadChart(args.stock.ticker, it) }
-            chart.marker =
-                MyMarkerView(requireContext(), R.layout.chart_marker, args.stock.currency, showTime)
-
-
-        }
-
-        chart.apply {
-            setScaleEnabled(false)
-            isDoubleTapToZoomEnabled = false
-            xAxis.isEnabled = false
-            axisLeft.isEnabled = false
-            axisRight.isEnabled = false
-            legend.isEnabled = false
-            description = Description().also { it.text = "" }
-            isAutoScaleMinMaxEnabled = true
-            marker = MyMarkerView(requireContext(), R.layout.chart_marker, args.stock.currency, showTime)
-        }
+        ivBack = view.findViewById(R.id.ivBackDetails)
 
         val tempStock = args.stock
 
         tvTicker.text = tempStock.ticker
         tvCompanyName.text = tempStock.companyName
-
-        tvCurrentPrice.text = toPriceString(tempStock.price, tempStock.currency, resources)
-        tvDayDelta.text = toDeltaString(
-            tempStock.dayDelta,
-            tempStock.dayDeltaPercent,
-            tempStock.currency,
-            resources
-        )
-
-        // Share price including commission
-        btnBuy.text = resources.getString(
-            R.string.btn_buy_text,
-            toPriceString(tempStock.price * COMMISSION_MULTIPLIER, tempStock.currency, resources)
-        )
-
-        if (tempStock.dayDelta < 0) {
-            tvDayDelta.setTextColor(ContextCompat.getColor(view.context, R.color.red))
-        } else {
-            tvDayDelta.setTextColor(ContextCompat.getColor(view.context, R.color.green))
-        }
         ivFav.visibility = if (tempStock.isFavorite) View.VISIBLE else View.INVISIBLE
 
+        viewPager.adapter = ViewPagerAdapter(this, args.stock)
+        TabLayoutMediator(tabLayout, viewPager, true) { tab, position ->
+            tab.setText(
+                when(position) {
+                    0 -> R.string.tab_chart
+                    1 -> R.string.tab_summary
+                    2 -> R.string.tab_news
+                    else -> throw IllegalArgumentException("$position is not registered tab")
+                }
+            )
+            viewPager.setCurrentItem(tab.position, true)
+        }.attach()
+
         ivBack.setOnClickListener {
-            it.findNavController().navigateUp()
+            findNavController().navigateUp()
         }
     }
 
 }
-
-const val COMMISSION_MULTIPLIER = 1.00083f
---
+++ app/src/main/java/com/zelyder/stocksapp/presentation/details/ViewPagerAdapter.kt
@@ -0,0 +1,20 @@
+package com.zelyder.stocksapp.presentation.details
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.zelyder.stocksapp.domain.models.Stock
+import java.lang.IllegalArgumentException
+
+class ViewPagerAdapter(fragment: Fragment, val stock: Stock) : FragmentStateAdapter(fragment){
+    override fun getItemCount(): Int = CARD_ITEM_SIZE
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0 -> ChartFragment.newInstance(stock)
+            1 -> SummaryFragment.newInstance()
+            2 -> NewsFragment.newInstance()
+            else -> throw IllegalArgumentException("$position is not registered tab")
+        }
+    }
+}
+private const val CARD_ITEM_SIZE = 3
